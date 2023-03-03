633c3d7d7f9a485586393633667f886705df2c4c
abdulbari149 <abdulbari131103@gmail.com>
2023-03-03 15:27:08 +0500

    auth signup login formatting

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,1 +1,3 @@
-export {}
+export {default as Layout} from "../auth/components/Layout"
+export {default as Login} from "../auth/components/login"
+export {default as Signin} from "../auth/components/signin"
