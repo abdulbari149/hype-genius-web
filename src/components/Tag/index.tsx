79db1f3379c0765d98c6208b43ff2af7bbde7b3f
abdulbari149 <abdulbari131103@gmail.com>
2023-03-04 17:00:56 +0500

    created a basic tag component

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,12 +1,30 @@
-import { TagType } from "@/modules/influencers/core/types";
 import React from "react";
 
-const Tag: React.FC<TagType> = ({ title, color }) => {
-	return (
-		<span className="px-4 py-2 rounded-xl text-[12px]" style={{ backgroundColor: color }}>
-			{title}
-		</span>
+type TagTypes = "danger" | "success";
+
+interface TagsProps extends React.HtmlHTMLAttributes<HTMLElement> {
+	as?: "div" | "p" | "button" | "a" | "section" | "main" | "header" | "span";
+	text: string;
+	tagType: TagTypes;
+}
+
+const tagColors: Record<TagTypes, string> = {
+	danger: "bg-[#FFCBD7]",
+	success: "bg-[#CAFFA0]",
+};
+const Tag: React.FC<TagsProps> = ({
+	as = "div",
+	text,
+	tagType,
+	className = "",
+	...props
+}) => {
+	const rootElement = React.createElement(
+		as,
+		{ ...props, className: `${className} ${tagColors[tagType]} px-4 py-2 rounded-[14px]` },
+		text
 	);
+	return rootElement;
 };
 
 export default Tag;
