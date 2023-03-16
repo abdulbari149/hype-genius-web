eda346d99815446ad947d844574fd3ee48f2e315
abdulbari149 <abdulbari131103@gmail.com>
2023-03-16 22:47:48 +0500

    feat: moved signup pages to /signup dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Poppins } from 'next/font/google'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Poppins } from "next/font/google";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [queryClient] = useState(() => new QueryClient());
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Component {...pageProps} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
