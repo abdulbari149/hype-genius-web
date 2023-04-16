7039df660e806d99ea82c7681eb68ae40abd27e6
abdulbari149 <abdulbari131103@gmail.com>
2023-04-16 23:38:32 +0500

    feat: link copied tooltip

--- src/modules/settings/components/SettingsOnboarding.tsx
+++ src/modules/settings/components/SettingsOnboarding.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMyBusiness } from '../hooks/useMyBusiness'
-
+import MdTooltip from '@mui/material/Tooltip'
 const SettingsOnboarding: React.FC = () => {
+	const [open, setOpen] = useState(false)
+
+	const handleTooltipClose = () => {
+		setOpen(false)
+	}
 	const { data } = useMyBusiness({})
+	const handleTooltipOpen = () => {
+		setOpen(true)
+		navigator.clipboard.writeText(data?.data?.onboardingLink ?? '')
+	}
+
+	useEffect(() => {
+		let timer: NodeJS.Timeout | undefined = undefined
+		if (open) {
+			timer = setTimeout(() => {
+				setOpen(false)
+			}, 500)
+		}
+		return () => {
+			if (timer && typeof timer !== 'undefined') {
+				clearTimeout(timer)
+			}
+		}
+	}, [open])
 
 	return (
 		<div
@@ -18,14 +41,24 @@ const SettingsOnboarding: React.FC = () => {
 					<p className="text-[#5C6FFF] w-fit cursor-pointer">
 						{data?.data.onboardingLink}
 					</p>
-					<button
-						onClick={() => {
-							navigator.clipboard.writeText(data?.data?.onboardingLink ?? '')
+					<MdTooltip
+						PopperProps={{
+							disablePortal: true,
 						}}
-						className="bg-[#EF539E] w-[60px] h-[31px] rounded-xl px-2 text-white ml-4 "
+						onClose={handleTooltipClose}
+						open={open}
+						disableFocusListener
+						disableHoverListener
+						disableTouchListener
+						title="Link Copied"
 					>
-						Copy
-					</button>
+						<button
+							onClick={handleTooltipOpen}
+							className="bg-[#EF539E] w-[60px] h-[31px] rounded-xl px-2 text-white ml-4 "
+						>
+							Copy
+						</button>
+					</MdTooltip>
 				</div>
 			</div>
 		</div>
